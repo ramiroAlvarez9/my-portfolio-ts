e508b3c2d607d397a236e73d7459563790451154
Ramiro Alvarez <ramiro.al.alvarez@gmail.com>
2024-01-29 13:55:08 -0300

    agregando youtube a seccion de videos. Api funcionando OK, pintando elementos en DOM

--- pages/musicvideos.tsx
+++ pages/musicvideos.tsx
@@ -4,31 +4,40 @@ import { getServerSideProps } from "../src/app/getServerSideProps";
 import { useEffect, useState } from "react";
 
 export default function MusicVideos() {
+
   const [isLoading, setIsLoading] = useState<Boolean>(true);
+
   const [youtubePlaylistItemsInfoProps, setYoutubePlaylistItemsInfoProps] =
     useState<Object | any>({});
 
-  const getApiInfo = async () =>
-    setYoutubePlaylistItemsInfoProps(await getServerSideProps());
+  const getApiInfo = async () =>  { 
+  
+    setYoutubePlaylistItemsInfoProps(await getServerSideProps() ) 
+    setIsLoading (false);
 
-  useEffect(() => {
-    getApiInfo();
-  }, []);
+  };
+
+  useEffect( () => { getApiInfo(); }, [] );
 
   return isLoading ? (
     <section id="ultimos-lanzamientos">
       <div className="ultimos-lanzamientos__container">
-        <h2>ULTIMOS LANZAMIENTOS</h2>
+        
         <h1>Cargando....</h1>
       </div>
     </section>
   ) : (
+    
     <section id="ultimos-lanzamientos">
+       <div className="ultimos-lanzamientos__container">
+      <h2>ULTIMOS LANZAMIENTOS</h2>
+
       <ArrayOfVideos
-        ArrayOfVideos={
-          youtubePlaylistItemsInfoProps.props.YoutubePlaylistItemsInfo.items
-        }
+        ArrayOfVideos={ youtubePlaylistItemsInfoProps.props.YoutubePlaylistItemsInfo.items }
       />
+      </div>
     </section>
+    
+
   );
 }
--- src/components/ArrayOfVideos.tsx
+++ src/components/ArrayOfVideos.tsx
@@ -3,10 +3,22 @@ interface Props{
 }
 export default function ArrayOfVideos({ArrayOfVideos}: Props) {
 
+  console.log(ArrayOfVideos)
+
+  const arrayOfVideos  = ArrayOfVideos.map( (object : Object | any, index: number ) => {
+      
+    console.log();
+    return(
+        <h3> {object.snippet.title}</h3>
+      );
+  });
+
+
 
   return (
-    <>     
-     
+    <>      
+
+     {arrayOfVideos}
     
     </>
   );
